06af49259986dc9f6d80f5a586fb4eaea9fedb25
PIYUSH <ynr24piyush@gmail.com>
2022-05-14 15:04:53 +0530

    VERSION 2

--- PYQT/FMDFR/GUI.py
+++ PYQT/FMDFR/GUI.py
@@ -5,11 +5,10 @@ from PyQt5.QtGui import QIcon, QImage, QPixmap, QCloseEvent
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtWidgets import QDialog, QApplication, QStackedWidget, QWidget
 from PySide2.QtCore import QFile
-# from PySide2.QtUiTools import QUiLoader
+
 #Face Recognition
 from importlib.resources import path
 from pydoc import classname
-from pyexpat import model
 import cv2
 import face_recognition
 import os
@@ -19,9 +18,6 @@ import numpy as np
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
-from imutils.video import VideoStream
-import imutils
-import time
 
 path = "K:\PROJECT\FACE_RECOGNIZE\IMAGES"
 
@@ -51,7 +47,7 @@ weightsPath = "K:/PROJECT/FaceMaskDetection-main/face_detector/res10_300x300_ssd
 faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
 
 # load the face mask detector model from disk
-maskNet = load_model("mask_detector.model")
+maskNet = load_model("K:\PROJECT\PYQT\FMDFR\mask_detector.model")
 
 print("Lets go")
 class MainScreen(QDialog):
@@ -59,26 +55,19 @@ class MainScreen(QDialog):
         super(MainScreen, self).__init__()
         
         path = os.fspath(Path(__file__).resolve().parent / "form.ui")
-        # ui_file = QFile(path)
         loadUi(path,self)
         self.FR.clicked.connect(self.F_R)
         self.FMD.clicked.connect(self.F_M_D)
-        # self.show()
-        # print(app.aboutToQuit.connect(self.closeEvent))
-        # self.back_from_FR.clicked.connect(self.__init__)
-        # self.back_from_FMD.clicked.connect(self.__init__)
     
     def F_R(self):
         fr=Face_Recognization()
         widget.addWidget(fr)
         widget.setCurrentIndex(widget.currentIndex()+1)
-        # fr.back_from_FR.clicked.connect(self.__init__)
     
     def F_M_D(self):
         fmd=Face_Mask_Detection()
         widget.addWidget(fmd)
         widget.setCurrentIndex(widget.currentIndex()+1)
-        # fmd.back_from_FMD.clicked.connect(self.__init__)
 
 class Face_Recognization(QDialog):
     def __init__(self):
@@ -101,8 +90,6 @@ class Face_Recognization(QDialog):
         widget.addWidget(M)
         widget.setCurrentIndex(widget.currentIndex()+1)
     def cap_img(self):
-        # cap = cv2.VideoCapture(0)
-        default=""
         while (self.cap.isOpened()):
             success, img = self.cap.read()
             if success == True:
@@ -125,9 +112,6 @@ class Face_Recognization(QDialog):
                     if matches[matchindex]:
                         name = classname[matchindex].upper()
                         self.NAME.setText(name)
-                        # cv2.waitKey()
-                # app.aboutToQuit.connect(self.close_all)
-                # print(app.aboutToQuit.connect(self.close_all))
     
     def display(self, img, window=1):
         qformat=QImage.Format_Indexed8
@@ -164,108 +148,63 @@ class Face_Mask_Detection(QDialog):
         widget.setCurrentIndex(widget.currentIndex()+1)
     
     def detect_and_predict_mask(self, frame, faceNet, maskNet):
-        # grab the dimensions of the frame and then construct a blob
-        # from it
         (h, w) = frame.shape[:2]
         blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224),
             (104.0, 177.0, 123.0))
 
-        # pass the blob through the network and obtain the face detections
         faceNet.setInput(blob)
         detections = faceNet.forward()
-        # print(detections.shape)
 
-        # initialize our list of faces, their corresponding locations,
-        # and the list of predictions from our face mask network
         faces = []
         locs = []
         preds = []
 
-        # loop over the detections
         for i in range(0, detections.shape[2]):
-            # extract the confidence (i.e., probability) associated with
-            # the detection
             confidence = detections[0, 0, i, 2]
-
-            # filter out weak detections by ensuring the confidence is
-            # greater than the minimum confidence
             if confidence > 0.5:
-                # compute the (x, y)-coordinates of the bounding box for
-                # the object
                 box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
                 (startX, startY, endX, endY) = box.astype("int")
 
-                # ensure the bounding boxes fall within the dimensions of
-                # the frame
                 (startX, startY) = (max(0, startX), max(0, startY))
                 (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
 
-                # extract the face ROI, convert it from BGR to RGB channel
-                # ordering, resize it to 224x224, and preprocess it
                 face = frame[startY:endY, startX:endX]
                 face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
                 face = cv2.resize(face, (224, 224))
                 face = img_to_array(face)
                 face = preprocess_input(face)
 
-                # add the face and bounding boxes to their respective
-                # lists
                 faces.append(face)
                 locs.append((startX, startY, endX, endY))
-        # only make a predictions if at least one face was detected
         if len(faces) > 0:
-            # for faster inference we'll make batch predictions on *all*
-            # faces at the same time rather than one-by-one predictions
-            # in the above `for` loop
             faces = np.array(faces, dtype="float32")
             preds = maskNet.predict(faces, batch_size=32)
 
-        # return a 2-tuple of the face locations and their corresponding
-        # locations
         return (locs, preds)
     
     def cap_img(self):
         while (self.cap.isOpened()):
-        # grab the frame from the threaded video stream and resize it
-        # to have a maximum width of 400 pixels
             success,frame = self.cap.read()
-            # frame = imutils.resize(frame, width=400)
             if success==True:
                 self.display(frame,1)
                 cv2.waitKey()
-                # detect faces in the frame and determine if they are wearing a
-                # face mask or not
+
                 imgs = cv2.resize(frame, (0, 0), None, 0.25, 0.25)
                 imgs = cv2.cvtColor(imgs, cv2.COLOR_BGR2RGB)
                 (locs, preds) = self.detect_and_predict_mask(imgs, faceNet, maskNet)
 
-                # loop over the detected face locations and their corresponding
-                # locations
                 app.aboutToQuit.connect(self.close_all)
                 for (box, pred) in zip(locs, preds):
-                    # unpack the bounding box and predictions
-                    # (startX, startY, endX, endY) = box
+
                     (mask, withoutMask) = pred
 
-                    # determine the class label and color we'll use to draw
-                    # the bounding box and text
                     label = "Mask" if mask > withoutMask else "No Mask"
                     color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
 
-                    # include the probability in the label
                     label = "{}".format(label)
                     print(label)
-                    # display the label and bounding box rectangle on the output
-                    # frame
-                    # cv2.putText(frame, label, (startX, startY - 10),
-                    #     cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-                    # cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
-                    self.REMARK.setText(label)
-                    
 
-            # show the output frame
-            # cv2.imshow("Frame", frame)
-            # key = cv2.waitKey(1) & 0xFF
+                    self.REMARK.setText(label)
     
     def display(self, img, window=1):
         qformat=QImage.Format_Indexed8
@@ -281,20 +220,20 @@ class Face_Mask_Detection(QDialog):
         self.rec_img.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
 
     
-
-app = QApplication(sys.argv)
-welcome=MainScreen()
-widget=QStackedWidget()
-widget.setWindowTitle("CHAKSHU")
-widget.setWindowIcon(QIcon("K:\PROJECT\PYQT\FMDFR\LOGO.png"))
-widget.addWidget(welcome)
-widget.setFixedHeight(600)
-widget.setFixedWidth(1200)
-widget.show()
-
-try:
-    sys.exit(app.exec_())
-except:
-    # q.cap.release()
-    # cv2.destroAllWindows()
-    print("Exiting")
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    welcome=MainScreen()
+    widget=QStackedWidget()
+    widget.setWindowTitle("CHAKSHU")
+    widget.setWindowIcon(QIcon("K:\PROJECT\PYQT\FMDFR\LOGO.png"))
+    widget.addWidget(welcome)
+    widget.setFixedHeight(600)
+    widget.setFixedWidth(1200)
+    widget.show()
+    app.exec()
+    # try:
+    #     sys.exit(app.exec_())
+    # except:
+    #     # q.cap.release()
+    #     # cv2.destroAllWindows()
+    #     print("Exiting")
